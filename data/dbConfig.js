b4db62feea0bcbd6533ddbfb0d24ac31ab7de3c8
David Horstman <davidzilla27@gmail.com>
2020-06-14 14:43:01 -0400

    Start building tests

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 
-const knexfile = require("../knexfile.js");
+const knexfile = require("../knexfile");
 const database = process.env.NODE_ENV || "development";
 
 module.exports = knex(knexfile[database]);
